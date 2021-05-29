a2e1a739ee17966797701e75d2325c7b74653925
Mkrolick <malcolm.krolick@gmail.com>
2021-05-29 14:59:12 -0400

    Making Base.html Work

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from myproject import app,db
 from flask import render_template, redirect, request, url_for, flash,abort, Response
 from flask_login import login_user,login_required,logout_user, current_user
-from myproject.models import User
+from myproject.models import User, Class, Books, Offers
 from werkzeug.security import generate_password_hash, check_password_hash
 from myproject.Login.forms import LoginForm
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
 
 
 @app.route('/')
@@ -12,10 +13,13 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/view_book')
+@app.route('/classes')
 @login_required
-def welcome_user():
-    return render_template('book_listings.html')
+def classes():
+    Classes = Class.query.all()
+    return render_template('classes.html', Classes=Classes)
+
+
 
 
 
